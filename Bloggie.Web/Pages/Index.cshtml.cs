6f144b790ed99cd32aa080d9492035d79c298863
John Kakouris <jdkakouris@gmail.com>
2023-07-01 17:52:22 +0300

    displaying home page

--- Bloggie.Web/Pages/Index.cshtml.cs
+++ Bloggie.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
 
 namespace Bloggie.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IBlogPostRepository blogPostRepository;
+        
+        public List<BlogPost> Blogs { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IBlogPostRepository blogPostRepository)
         {
             _logger = logger;
+            this.blogPostRepository = blogPostRepository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            Blogs = (await blogPostRepository.GetAllAsync()).ToList();   
+            return Page();
         }
     }
 }
