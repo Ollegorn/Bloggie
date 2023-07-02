ebf3806a0c6cb424e879d11169f62e6943fb9b6e
John Kakouris <jdkakouris@gmail.com>
2023-07-02 19:44:40 +0300

    tag changes in edit page

--- Bloggie.Web/Pages/Admin/Blogs/Edit.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -17,6 +17,8 @@ namespace Bloggie.Web.Pages.Admin.Blogs
 
         [BindProperty]
         public IFormFile FeaturedImage { get; set; }
+        [BindProperty]
+        public string Tags { get; set; }
 
         public EditModel(IBlogPostRepository blogPostRepository)
         {
@@ -26,12 +28,19 @@ namespace Bloggie.Web.Pages.Admin.Blogs
         public async Task OnGet(Guid id)
         {
              BlogPost = await blogPostRepository.GetAsync(id);
+
+            if (BlogPost !=null && BlogPost.Tags != null)
+            {
+                Tags = string.Join(',', BlogPost.Tags.Select(x => x.Name));
+            }
             
         }
         public async Task<IActionResult> OnPostEdit()
         {
             try
             {
+                BlogPost.Tags = new List<Tag>(Tags.Split(',').Select(x => new Tag() { Name = x.Trim() }));
+
                 await blogPostRepository.UpdateAsync(BlogPost);
 
                 ViewData["Notification"] = new Notification
