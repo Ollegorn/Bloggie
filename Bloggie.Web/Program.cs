5ae6f77d033d47c6ccf59e6433fc7cddae199189
John Kakouris <jdkakouris@gmail.com>
2023-07-03 17:45:42 +0300

    adding connection string and injecting dbcontext in program.cs

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -1,5 +1,6 @@
 using Bloggie.Web.Data;
 using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
-
+//inject db context
 builder.Services.AddDbContext<BloggieDbContext>(options=>
-options.UseSqlServer(
+ options.UseSqlServer(
     builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
+//inject auth db context
+builder.Services.AddDbContext<AuthDbContext>(options =>
+ options.UseSqlServer(
+    builder.Configuration.GetConnectionString("BloggieAuthDbConnectionString")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDbContext>();
 
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepositoryCloudinary>();
@@ -31,6 +39,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
