7565864a155d629938beeec7bb2cc59ed0cfb623
John Kakouris <jdkakouris@gmail.com>
2023-07-03 18:50:57 +0300

    implementing register page

---
+++ Bloggie.Web/Pages/Register.cshtml.cs
@@ -0,0 +1,53 @@
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.Web.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [BindProperty]
+        public Register RegisterViewModel { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost() 
+        {
+            var user = new IdentityUser
+            {
+                UserName = RegisterViewModel.Username,
+                Email = RegisterViewModel.Email
+            };
+
+            var identityResult = await userManager.CreateAsync(user,RegisterViewModel.Password);
+
+            if (identityResult.Succeeded)
+            {
+                ViewData["Notification"] = new Notification
+                {
+                    Type = Enums.NotificationType.Success,
+                    Message = "Succesfull registration"
+                };
+
+                return Page();
+            }
+
+            ViewData["Notification"] = new Notification
+            {
+                Type = Enums.NotificationType.Error,
+                Message = "Something went wrong"
+            };
+            return Page();
+        }
+
+    }
+}
