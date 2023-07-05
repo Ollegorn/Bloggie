dd25ef7d2bbd51ea9c484b55f1d054108affbf2a
John Kakouris <jdkakouris@gmail.com>
2023-07-05 16:22:03 +0300

    total likes link with db + implementation

--- Bloggie.Web/Pages/Blog/Details.cshtml.cs
+++ Bloggie.Web/Pages/Blog/Details.cshtml.cs
@@ -8,16 +8,25 @@ namespace Bloggie.Web.Pages.Blog
     public class DetailsModel : PageModel
     {
         private readonly IBlogPostRepository blogPostRepository;
-        public BlogPost BlogPost { get; set; }
+        private readonly IBlogPostLikeRepository blogPostLikeRepository;
 
-        public DetailsModel(IBlogPostRepository blogPostRepository)
+        public BlogPost BlogPost { get; set; }
+        public int TotalLikes { get; set; }
+
+        public DetailsModel(IBlogPostRepository blogPostRepository,IBlogPostLikeRepository blogPostLikeRepository)
         {
             this.blogPostRepository = blogPostRepository;
+            this.blogPostLikeRepository = blogPostLikeRepository;
         }
 
         public async Task<IActionResult> OnGet(string urlHandle)
         {
             BlogPost = await blogPostRepository.GetAsync(urlHandle);
+            if (BlogPost != null)
+            {
+                TotalLikes = await blogPostLikeRepository.GetTotalLikesForBlog(BlogPost.Id);
+            }
+
             return Page();
         }
     }
