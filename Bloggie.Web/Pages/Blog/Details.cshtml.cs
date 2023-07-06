aa736627a8e75ad46bee192344e42f4200367b7e
John Kakouris <jdkakouris@gmail.com>
2023-07-06 16:38:44 +0300

    comment section structure + functionality

--- Bloggie.Web/Pages/Blog/Details.cshtml.cs
+++ Bloggie.Web/Pages/Blog/Details.cshtml.cs
@@ -12,21 +12,28 @@ namespace Bloggie.Web.Pages.Blog
         private readonly IBlogPostLikeRepository blogPostLikeRepository;
         private readonly SignInManager<IdentityUser> signInManager;
         private readonly UserManager<IdentityUser> userManager;
+        private readonly IBlogPostCommentRepository blogPostCommentRepository;
 
         public BlogPost BlogPost { get; set; }
         public int TotalLikes { get; set; }
 
         public bool IsLiked { get; set; }
+        [BindProperty]
+        public Guid BlogPostId { get; set; }
+        [BindProperty]
+        public string CommentDescription { get; set; }
 
-        public DetailsModel(IBlogPostRepository blogPostRepository
-            ,IBlogPostLikeRepository blogPostLikeRepository
-            ,SignInManager<IdentityUser> signInManager
-            ,UserManager<IdentityUser> userManager)
+        public DetailsModel(IBlogPostRepository blogPostRepository,
+            IBlogPostLikeRepository blogPostLikeRepository,
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            IBlogPostCommentRepository blogPostCommentRepository)
         {
             this.blogPostRepository = blogPostRepository;
             this.blogPostLikeRepository = blogPostLikeRepository;
             this.signInManager = signInManager;
             this.userManager = userManager;
+            this.blogPostCommentRepository = blogPostCommentRepository;
         }
 
         public async Task<IActionResult> OnGet(string urlHandle)
@@ -34,6 +41,7 @@ namespace Bloggie.Web.Pages.Blog
             BlogPost = await blogPostRepository.GetAsync(urlHandle);
             if (BlogPost != null)
             {
+                BlogPostId = BlogPost.Id;
                 if (signInManager.IsSignedIn(User))
                 {
                     var likes = await blogPostLikeRepository.GetLikesForBlog(BlogPost.Id);
@@ -47,6 +55,27 @@ namespace Bloggie.Web.Pages.Blog
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPost(string urlHandle)
+        {
+            if (signInManager.IsSignedIn(User) && !string.IsNullOrWhiteSpace(CommentDescription))
+            {
+                var userId = userManager.GetUserId(User);
+
+                var comment = new BlogPostComment()
+                {
+                    BlogPostId = BlogPostId,
+                    Description = CommentDescription,
+                    DateAdded = DateTime.Now,
+                    UserId = Guid.Parse(userId)
+                };
+                await blogPostCommentRepository.AddAsync(comment);
+            }
+
+            return RedirectToPage("/Blog/Details", new {urlHandle = urlHandle });
+
+
         }
     }
 }
