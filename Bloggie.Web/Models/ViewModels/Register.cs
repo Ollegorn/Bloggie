8a629f460ec32c99c1e3faba0486ee1781e859d3
John Kakouris <jdkakouris@gmail.com>
2023-07-10 14:30:28 +0300

    client side validations for register page

--- Bloggie.Web/Models/ViewModels/Register.cs
+++ Bloggie.Web/Models/ViewModels/Register.cs
@@ -8,9 +8,9 @@ namespace Bloggie.Web.Models.ViewModels
         public string Username { get; set; }
         [Required]
         [EmailAddress]
-        public string Password { get; set; }
+        public string Email { get; set; }
         [Required]
         [MinLength(6)]
-        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
